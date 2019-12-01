2772dc13895f4f90f602c31627c9fd25df188313
bioshiki <55005595+bioshiki@users.noreply.github.com>
2019-11-30 23:55:11 -0500

    Add files via upload

---
+++ oop345/w9/SecureData.cpp
@@ -0,0 +1,133 @@
+// Workshop 9 - Multi-Threading
+// SecureData.cpp
+// Name: Xingjian Yuan
+// Seneca Student ID: 126105188
+// Seneca email: xyuan22@myseneca.ca
+// Date of completion: 2019-11-26
+//
+// I confirm that the content of this file is created by me,
+//   with the exception of the parts provided to me by my professor.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <thread>
+#include <functional>
+#include "SecureData.h"
+
+using namespace std;
+
+namespace w9 {
+
+	void converter(char* t, char key, int n, const Cryptor& c) {
+		for (int i = 0; i < n; i++)
+			t[i] = c(t[i], key);
+	}
+
+	SecureData::SecureData(const char* file, char key, ostream* pOfs)
+	{
+		ofs = pOfs;
+
+		// open text file
+		fstream input(file, std::ios::in);
+		if (!input)
+			throw string("\n***Failed to open file ") +
+			string(file) + string(" ***\n");
+
+		// copy from file into memory
+		input.seekg(0, std::ios::end);
+		nbytes = (int)input.tellg() + 1;
+
+		text = new char[nbytes];
+
+		input.seekg(ios::beg);
+		int i = 0;
+		input >> noskipws;
+		while (input.good())
+			input >> text[i++];
+		text[nbytes - 1] = '\0';
+		*ofs << "\n" << nbytes - 1 << " bytes copied from file "
+			<< file << " into memory (null byte added)\n";
+		encoded = false;
+
+		// encode using key
+		code(key);
+		*ofs << "Data encrypted in memory\n";
+	}
+
+	SecureData::~SecureData() {
+		delete[] text;
+	}
+
+	void SecureData::display(std::ostream& os) const {
+		if (text && !encoded)
+			os << text << std::endl;
+		else if (encoded)
+			throw std::string("\n***Data is encoded***\n");
+		else
+			throw std::string("\n***No data stored***\n");
+	}
+
+	void SecureData::code(char key)
+	{
+		// TODO (at-home): rewrite this function to use at least two threads
+		//         to encrypt/decrypt the text.
+		//converter(text, key, nbytes, Cryptor());
+		auto p1 = std::bind(converter, text, key, (int)nbytes / 2, Cryptor());
+		auto p2 = std::bind(converter, &text[(int)nbytes / 2], key, nbytes - (int)nbytes / 2, Cryptor());
+		std::thread t1(p1);
+		std::thread t2(p2);
+		encoded = !encoded;
+		t1.join();
+		t2.join();
+	}
+
+	void SecureData::backup(const char* file) {
+		if (!text)
+			throw std::string("\n***No data stored***\n");
+		else if (!encoded)
+			throw std::string("\n***Data is not encoded***\n");
+		else
+		{
+			// TODO: open a binary file for writing
+			std::ofstream of(file, std::ios::binary);
+
+			// TODO: write data into the binary file
+			//         and close the file
+			of.write(text, nbytes);
+			of.close();
+		}
+	}
+
+	void SecureData::restore(const char* file, char key) {
+		// TODO: open binary file for reading
+		std::ifstream in(file, std::ios::binary);
+		if (!in) {
+			throw std::string("unable to open file for restore");
+		}
+		// TODO: - allocate memory here for the file content
+		in.seekg(0, std::ios::end);
+		nbytes = (int)in.tellg()+1;
+		if (text != nullptr)
+			delete text;
+		text = new char[nbytes];
+		in.seekg(0);
+		// TODO: - read the content of the binary file
+		in.read(text, nbytes-1);
+		text[nbytes-1] = '\0';
+		*ofs << "\n" << nbytes << " bytes copied from binary file "
+			<< file << " into memory.\n";
+
+		encoded = true;
+
+		// decode using key
+		code(key);
+
+		*ofs << "Data decrypted in memory\n\n";
+	}
+
+	std::ostream& operator<<(std::ostream& os, const SecureData& sd) {
+		sd.display(os);
+		return os;
+	}
+}
